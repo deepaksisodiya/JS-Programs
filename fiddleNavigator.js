fd07dfeb12363c826cfe10247d5fedff11e01a63
deepak sisodiya <deepak.sisodiya@gmail.com>
2014-12-04 10:39:21 +0530

    added click handler

--- fiddleNavigator.js
+++ fiddleNavigator.js
@@ -10,26 +10,26 @@ function getRequest(file, callback) {
 }
 
 var dataConfig;
-getRequest("config.json", function () {
-    dataConfig = JSON.parse(this.responseText);
-    common(dataConfig[0], "iframeTemplate", "section");
-    common(dataConfig, "navBar", "nav");
-});
-
 function common(dataObj, tempId, tagId){
     var tempStr = $("#" + tempId).html();
     var compileTemp = _.template(tempStr);
     var tempHTML = compileTemp({
         obj:dataObj
-    })
+    });
     $(tagId).html(tempHTML);
 }
 
-function loadConfig(index){
-    var tempStr = $("#iframeTemplate").html();
-    var compileTemp = _.template(tempStr);
-    var tempHTML = compileTemp({
-        obj:dataConfig[index]
-    })
-    $("section").html(tempHTML);
-}
+getRequest("config.json", function () {
+    dataConfig = JSON.parse(this.responseText);
+    common(dataConfig, "navBar", "nav");
+    $(".link").click(function () {
+        var index = this.getAttribute("data-index");//var index = $(this).attr("data-index");//var index = $(this).data("index");//var index = this.dataset.index
+        common(dataConfig[index], "iframeTemplate", "section");
+    });
+    $(".link:first").trigger("click");
+});
+
+//When whole template is loaded, click the first one//
+//Do not make global variable
+//split whole JS code in two components
+//
